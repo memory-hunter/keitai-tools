ab33b710955290af805272f2ebf96bac81218ed9
kagekiyo <WoW@example.com>
2025-02-15 18:31:05 +0900

    Improved verify_jar

--- util/verify.py
+++ util/verify.py
@@ -5,10 +5,11 @@ def verify_jar(jar_data):
     if jar_data[0:4] != b"PK\x03\x04":
         return False
     
-    jar_stream = io.BytesIO(jar_data)
-    
-    with zipfile.ZipFile(jar_stream, "r") as f:
-        return f.testzip() is None
+    try:
+        with io.BytesIO(jar_data) as jar_stream, zipfile.ZipFile(jar_stream, "r") as f:
+            return f.testzip() is None
+    except zipfile.BadZipFile:
+        return False
 
 def verify_sp(spsize, jam_spsize_str):
     jam_spsize = sum([int(n) for n in jam_spsize_str.split(",")])
