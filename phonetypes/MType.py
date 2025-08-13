95ee2431be8d4e22464f35f57ef82109695852a8
memory-hunter <59766692+memory-hunter@users.noreply.github.com>
2025-08-14 01:23:29 +0200

    Add Motorola type prototype

---
+++ phonetypes/MType.py
@@ -0,0 +1,28 @@
+from phonetypes.PhoneType import PhoneType
+
+class MType(PhoneType):
+    """
+    A class to represent Motorola phone.
+    
+    Description:
+    - Contains trjava.log, J2MEST.SYS and USR files
+    - .adf file for JAM, .jar for JAR, .rms for SP files. SP files have headers already. ADF is in plaintext
+    """
+    
+    def extract(self, top_folder_directory, verbose=False):
+        """
+        Extract games from the top folder directory in a M phone.
+        
+        :param top_folder_directory: Top folder directory to extract games from.
+        """
+        pass
+    
+    def test_structure(self, top_folder_directory):
+        """
+        Test the structure of the top folder directory to see if it is an M type.
+        
+        :param top_folder_directory: Top folder directory to test the structure of.
+        
+        :return: True if at least one subfolder satisfies the D/F type structure, False otherwise.
+        """
+        pass
